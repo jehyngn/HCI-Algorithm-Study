5c565fa9dd010f32cae1beadfee3920e111c7198
jehyngn <106136905+jehyngn@users.noreply.github.com>
2022-09-06 17:56:27 +0900

    B3

---
+++ baekjoon/2562.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// https://www.acmicpc.net/problem/2562
+
+using namespace std;
+
+int main(void){
+    int max = 0;
+    int input = 0;
+    int index;
+    for(int i=0;i<9;i++){
+        cin >> input;
+        if(max < input){
+            max = input;
+            index = i;
+        }
+    }
+    cout << max << "\n" << index+1;
+    return 0;
+}
