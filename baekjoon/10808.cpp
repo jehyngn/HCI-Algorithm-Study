51e4bbdd44239b58047eeb69f94f024894a216f6
jehyngn <106136905+jehyngn@users.noreply.github.com>
2022-09-06 18:22:05 +0900

    B3

---
+++ baekjoon/10808.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    int minVal = arr[0];
+    int maxVal = arr[0];
+
+    for (int i=0;i<n;i++){
+        minVal = min(minVal, arr[i]);
+        maxVal = max(maxVal, arr[i]);
+    }
+    cout << minVal << " " << maxVal;
+    return 0;
+}
