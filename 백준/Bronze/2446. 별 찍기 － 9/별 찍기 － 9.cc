18b1a2c2f505986b9de7d44761f6d0b6a96bb056
Park JaeHyeong <106136905+jehyngn@users.noreply.github.com>
2023-01-11 12:02:39 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<n-i+1;j++){ // 0 1 2 3 4
+            cout << ' ';
+        }
+        for(int k=0;k<2*i-1;k++){ // 1 3 5 7 9
+            cout << '*';
+        }
+        cout << '\n';
+    }
+
+    
+    for(int i=2;i<=n;i++){
+        for(int j=1;j<n-i+1;j++){ // 0 1 2 3 4
+            cout << ' ';
+        }
+        for(int k=0;k<2*i-1;k++){ // 1 3 5 7 9
+            cout << '*';
+        }
+        cout << '\n';
+    }
+}
